e2a4f1b3bceebb0625e813541c7116053efa7ffb
Rabia-Rajput <rajput.rabia@outlook.com>
2024-02-21 16:13:55 +0100

    new changes are made

--- scripts/game.js
+++ scripts/game.js
@@ -1,6 +1,30 @@
 function startNewGame(){
-    if(players[0].name === ''  || players[1].name === '')
+    if(players[0].name === ''  || players[1].name === ''){ 
     alert('please set name for both players');
     return;
-    gameAreaElement.style.display = 'block';
+}
+activePlayerNameElement.textContent = players[activePlayer].name;
+
+gameAreaElement.style.display = 'block';
+  }
+function switchPlayer(){
+    if(activePlayer === 0) { 
+    activePlayer = 1;
+     }
+ else {
+    activePlayer = 0;
+}
+activePlayerNameElement.textContent = players[activePlayer].name;
+  }
+
+function selectGameField(event){
+    if (event.target.tagName !== 'LI'){
+        return;
+    }
+    event.target.textContent = players[activePlayer].symbol;
+    event.target.classList.add('disabled');
+
+    
+    switchPlayer();
+
 }
