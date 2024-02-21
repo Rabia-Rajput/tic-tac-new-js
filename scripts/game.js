8f83ac0afef2e5ab1f49845efd18c60e562f4d44
Rabia-Rajput <rajput.rabia@outlook.com>
2024-02-21 17:14:23 +0100

    new changes are made

--- scripts/game.js
+++ scripts/game.js
@@ -21,10 +21,55 @@ function selectGameField(event){
     if (event.target.tagName !== 'LI'){
         return;
     }
-    event.target.textContent = players[activePlayer].symbol;
-    event.target.classList.add('disabled');
+    const selectedField = event.target;
+    const selectedColumn = selectedField.dataset.col - 1;
+    const selectedRow = selectedField.dataset.row - 1;
+    if (gameData [selectedRow][selectedColumn] > 0){
+        //  alert('please select an empty field')
+        return;
+    }
 
-    
+    selectedField.textContent = players[activePlayer].symbol;
+    selectedField.classList.add('disabled');
+    const winnerId = checkGameOver();
+    console.log(winnerId);
+
+    gameData [selectedRow][selectedColumn] = activePlayer + 1;
+
+    //console.log(gameData);
     switchPlayer();
 
 }
+
+function checkGameOver(){
+    //checking the rows for equality
+    for ( let i = 0; i<3; i++){
+    if(gameData[i][0] > 0 &&  
+        gameData[i][0] === gameData [i][1] &&
+         gameData[i][1] === gameData[i][2] ){
+            return gameData[i][0];
+
+
+        }
+    }
+    //checking the columns for equality
+    for ( let i = 0; i<3; i++){
+        if(gameData[0][i] > 0 &&  
+            gameData[0][i] === gameData [1][i] &&
+             gameData[0][i] === gameData[2][i] ){
+                return gameData[0][i];
+    
+    
+           
+            } 
+         }
+         //Diagonal : top left to bottom right
+         if (gameData[2][0] >0 &&
+            gameData[2][0] === gameData[1][1] &&
+            gameData[1][1] === gameData[0][2] ){
+                return gameData[2][0];
+
+            }
+            return 0;
+        }
+     
